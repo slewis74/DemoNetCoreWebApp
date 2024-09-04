8510d32a5c8514c957b79b2c41c359c79624eac0
Shannon Lewis <slewis74@gmail.com>
2024-09-04 17:15:24 +1000

    Update DemoClass.cs

--- source/DemoClassLibrary/DemoClass.cs
+++ source/DemoClassLibrary/DemoClass.cs
@@ -2,7 +2,7 @@
 
 namespace DemoClassLibrary
 {
-    // Added something that might need code review..,,zzaabb
+    // Added something that might need code review..,,zzaabbcc
     public class DemoClass
     {
         public string SampleText => "Some sample text";
